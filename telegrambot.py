d8894413764b0eff15709310af2f0950a8874287
menezesalexandre_dev <alexandremmnzssilva2509@gmail.com>
2023-10-30 21:55:18 -0300

    COMMIT2 - 2023/10/30

---
+++ telegrambot.py
@@ -0,0 +1,30 @@
+import telebot
+
+chaveAPI = "6706522782:AAGZDff-S-L2QNsHqzGgohORMCN6EGNsnAY"
+bot = telebot.TeleBot(chaveAPI)
+
+@bot.message_handler(commands=["alemenezes"])
+def alemenezes(mensagem):
+    texto_alemenezes = """
+    Saiba mais sobre o meu trabalho!
+menezesalexandre_dev 👨‍💻 Programador Full-Stack
+HTML | CSS | JavaScript | Python | MySQL | Git
+GitHub: https://encurtador.com.br/qGHO3
+LinkedIn: https://encurtador.com.br/bkqt2"""
+    bot.reply_to(mensagem, texto_alemenezes)
+
+#MENSAGENS GERAIS:
+def verificar(mensagem):
+   return True
+
+@bot.message_handler(func=verificar)
+def responder(mensagem):
+    texto = """
+    Selecione uma opção para continuar (Clique no item):
+/brasileirao Ver tabela do Brasileirão 
+/dataclima Ver a data e clima de hoje
+/alemenezes Ver mais do trabalho de menezesalexandre_dev
+Selecione uma das opções disponíveis acima"""
+    bot.reply_to(mensagem, texto)
+
+bot.polling()
